ee9c359003554ad6ba443f6be9669e6ffedd5a34
Mert Eray <meerteray@gmail.com>
2024-07-26 16:56:43 +0300

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Mert Eray on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
