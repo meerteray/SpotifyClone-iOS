7aff4ab67c905cc05ea56891b6e26a1001e63286
Mert Eray <meerteray@gmail.com>
2024-07-27 20:25:28 +0300

    improved design

--- Spotify/ContentView.swift
+++ Spotify/ContentView.swift
@@ -9,7 +9,7 @@ struct ContentView: View {
                 Image("spotifyLogo")
                     .resizable()
                     .renderingMode(.template)
-                       .foregroundColor(.white)
+                    .foregroundColor(.white)
                     .frame(width: 50, height: 50)
                 
                 Text("Millions of songs.")
@@ -38,17 +38,24 @@ struct ContentView: View {
                 .padding(.bottom, 10)
                 
                 Button(action: {
-                   
+                    
                 }) {
                     HStack {
-                        Image(systemName: "g.circle.fill")
-                            .foregroundColor(.green)
+                        Image("googleLogo") // Google logosunu assets'e ekleyin ve adını burada belirtin
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                        Spacer()
                         Text("Continue with Google")
                             .font(.headline)
                             .foregroundColor(.white)
+                        Spacer()
                     }
                     .padding()
                     .frame(maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
                     .background(Color.black.opacity(0.8))
                     .cornerRadius(25)
                     .padding(.horizontal, 40)
@@ -58,14 +65,21 @@ struct ContentView: View {
                 Button(action: {
                 }) {
                     HStack {
-                        Image(systemName: "f.cursive.circle.fill")
-                            .foregroundColor(.blue)
+                        Image("facebookLogo") // Facebook logosunu assets'e ekleyin ve adını burada belirtin
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                        Spacer()
                         Text("Continue with Facebook")
                             .font(.headline)
                             .foregroundColor(.white)
+                        Spacer()
                     }
                     .padding()
                     .frame(maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
                     .background(Color.black.opacity(0.8))
                     .cornerRadius(25)
                     .padding(.horizontal, 40)
@@ -75,14 +89,21 @@ struct ContentView: View {
                 Button(action: {
                 }) {
                     HStack {
-                        Image(systemName: "applelogo")
-                            .foregroundColor(.white)
+                        Image("appleLogo") // Apple logosunu sistem simgesi olarak kullanıyoruz
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                        Spacer()
                         Text("Continue with Apple")
                             .font(.headline)
                             .foregroundColor(.white)
+                        Spacer()
                     }
                     .padding()
                     .frame(maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
                     .background(Color.black.opacity(0.8))
                     .cornerRadius(25)
                     .padding(.horizontal, 40)
@@ -96,6 +117,7 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .padding(.top, 10)
                 }
+                
                 Spacer()
             }
             .background(Color.black.edgesIgnoringSafeArea(.all))
