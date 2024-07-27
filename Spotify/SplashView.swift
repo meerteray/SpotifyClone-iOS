1d9372018b2132f79422761ba0d5d41f108663a8
Mert Eray <meerteray@gmail.com>
2024-07-27 09:19:47 +0300

    SplashView added

---
+++ Spotify/SplashView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Image("spotifyLogo")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .scaleEffect(size)
+                        .opacity(opacity)
+                        .onAppear {
+                            withAnimation(.easeIn(duration: 1.2)) {
+                                self.size = 1.0
+                                self.opacity = 1.0
+                            }
+                        }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
--- Spotify/SpotifyApp.swift
+++ Spotify/SpotifyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SpotifyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView() // Uygulama SplashView ile başlatılır
         }
     }
 }
