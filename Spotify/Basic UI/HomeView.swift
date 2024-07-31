81d8c65e3de3aa3285f88db33f74b340a01c7c62
Mert Eray <meerteray@gmail.com>
2024-07-31 13:22:01 +0300

    buttons added

--- Spotify/Basic UI/HomeView.swift
+++ Spotify/Basic UI/HomeView.swift
@@ -21,18 +21,45 @@ struct HomeView: View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             
-            VStack {
-                HStack {
-                    Text(greetingMessage())
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .bold()
-                    Spacer()
+            VStack(alignment: .leading, spacing: 20) {
+                Text(greetingMessage())
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .bold()
+                
+                VStack(spacing: 10) {
+                    HStack(spacing: 10) {
+                        createButton(title: "This Is Sezen Aksu", action: { print("Button 1 tapped") })
+                        createButton(title: "Liked Songs", action: { print("Button 2 tapped") })
+                    }
+                    HStack(spacing: 10) {
+                        createButton(title: "No.1", action: { print("Button 3 tapped") })
+                        createButton(title: "This Is Eminem", action: { print("Button 4 tapped") })
+                    }
+                    HStack(spacing: 10) {
+                        createButton(title: "This Is Kanye West", action: { print("Button 5 tapped") })
+                        createButton(title: "Ceza", action: { print("Button 6 tapped") })
+                    }
+                    HStack(spacing: 10) {
+                        createButton(title: "Sagopa Kajmer", action: { print("Button 7 tapped") })
+                        createButton(title: "Sefo", action: { print("Button 8 tapped") })
+                    }
                 }
-                .padding()
                 
                 Spacer()
             }
+            .padding()
+        }
+    }
+    
+    func createButton(title: String, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            Text(title)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.opacity(0.3))
+                .foregroundColor(.white)
+                .cornerRadius(10)
         }
     }
 }
