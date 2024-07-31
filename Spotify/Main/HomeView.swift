ba41e1d64a686525ee8d1c3eaf8a6f44f8fb71f3
Mert Eray <meerteray@gmail.com>
2024-07-31 12:44:11 +0300

    greeting message

--- Spotify/Main/HomeView.swift
+++ Spotify/Main/HomeView.swift
@@ -1,13 +1,40 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    func greetingMessage() -> String {
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        switch hour {
+        case 6..<12:
+            return "Good Morning"
+        case 12..<17:
+            return "Good Afternoon"
+        case 17..<22:
+            return "Good Evening"
+        default:
+            return "Good Night"
+        }
+    }
+    
     var body: some View {
-            ZStack {
-                Color.black.edgesIgnoringSafeArea(.all) 
-                Text("Home View")
-                    .foregroundColor(.white)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text(greetingMessage())
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                
+                Spacer()
             }
         }
+    }
 }
 
 #Preview {
