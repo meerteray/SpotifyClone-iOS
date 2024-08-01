997c85c2dc4e2c480bef4680d12f52e68b7b8be3
Mert Eray <meerteray@gmail.com>
2024-08-01 21:39:21 +0300

    no1 view ui improved

--- Spotify/Playlists/no1View.swift
+++ Spotify/Playlists/no1View.swift
@@ -2,9 +2,34 @@ import SwiftUI
 
 struct no1View: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(1), Color.black]),
+                           startPoint: .top,
+                           endPoint: .init(x: 0.6, y: 0.6))
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        ZStack {
+                            Image("no1Image")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 250, height: 250)
+                                .clipped()
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding()
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
+
 
 #Preview {
     no1View()
