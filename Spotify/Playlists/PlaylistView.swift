a2d755d56d3c81a2c9b3d0181684a746ef72a350
Mert Eray <meerteray@gmail.com>
2024-09-13 23:26:38 +0300

    play song

--- Spotify/Playlists/PlaylistView.swift
+++ Spotify/Playlists/PlaylistView.swift
@@ -34,11 +34,15 @@ struct PlaylistView: View {
                         }
                         ForEach(selectedUser.songs) { song in
                             Button(action: {
-                                playSong(song)
+                                playOrPauseSong(song)
                             }) {
-                                Text(song.name)
-                                    .foregroundColor(.white)
-                                    .padding(.vertical, 4)
+                                HStack {
+                                    Text(song.name)
+                                        .foregroundColor(currentSong?.id == song.id ? .green : .white)
+                                        .padding(.vertical, 4)
+                                    
+                                    Spacer()
+                                }
                             }
                         }
                     }
@@ -59,9 +63,17 @@ struct PlaylistView: View {
     
     private var playerControls: some View {
         HStack {
-            if let currentSong = currentSong {
+            if let currentSong = currentSong, let userImage = image {
+                Image(uiImage: userImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 44, height: 44)
+                    .clipShape(Circle())
+                    .padding(.trailing, 8)
+                
                 Text(currentSong.name)
                     .foregroundColor(.white)
+                    .font(.headline)
             }
             Spacer()
             Button(action: {
@@ -87,7 +99,7 @@ struct PlaylistView: View {
         let storage = Storage.storage()
         let reference = storage.reference(forURL: selectedUser.imageURL)
         
-        reference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+        reference.getData(maxSize: 10 * 1024 * 1024) { data, error in
             if let error = error {
                 print("Error downloading image: \(error.localizedDescription)")
             } else if let data = data, let uiImage = UIImage(data: data) {
@@ -98,22 +110,47 @@ struct PlaylistView: View {
         }
     }
     
+    private func playOrPauseSong(_ song: Song) {
+        if currentSong?.id == song.id {
+            if isPlaying {
+                audioPlayer?.pause()
+            } else {
+                audioPlayer?.play()
+            }
+            isPlaying.toggle()
+        } else {
+            stopCurrentSong()
+            playSong(song)
+        }
+    }
+    
+    private func stopCurrentSong() {
+        audioPlayer?.stop()
+        audioPlayer = nil
+        isPlaying = false
+    }
+    
+    
     private func playSong(_ song: Song) {
-        guard let url = URL(string: song.song) else {
-            print("Invalid song URL")
+        guard let url = URL(string: song.song.removingPercentEncoding ?? song.song) else {
+            print("Invalid song URL: \(song.song)")
             return
         }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        print("Playing song from URL: \(url)")
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("Error downloading song: \(error.localizedDescription)")
                 return
             }
             
             guard let data = data else {
-                print("No data received")
+                print("No data received for song: \(song.name)")
                 return
             }
+            
+            print("Downloaded data size: \(data.count) bytes")
             
             do {
                 audioPlayer = try AVAudioPlayer(data: data)
@@ -129,16 +166,3 @@ struct PlaylistView: View {
     }
 }
 
-#Preview {
-    PlaylistView(
-        selectedUser:
-            User(id: "1", name: "Sezen Aksu", imageURL:"https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Kaybolan%20Yıllar.mp3?alt=media&token=8266e208-763a-4df3-a108-735ef48daefa",
-                 songs: [
-                    Song(id: "1", name: "Gülümse", song: "https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Gülümse.mp3?alt=media&token=ebdfe903-3194-4ae5-b4c4-839fe9b8b5a5"),
-                    Song(id: "2", name: "Firuze", song: "https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Firuze.mp3?alt=media&token=bf01d723-aff9-4cad-8d66-036ff2037e80"),
-                    Song(id: "3", name: "Kaybolan Yıllar", song:"https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Kaybolan%20Yıllar.mp3?alt=media&token=8266e208-763a-4df3-a108-735ef48daefa"),
-                    Song(id: "4", name: "Kutlama", song: "https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Kutlama.mp3?alt=media&token=46348393-d9df-48b5-af72-c187c855d2b9"),
-                    Song(id: "5", name: "Seni Yerler", song: "https://firebasestorage.googleapis.com/v0/b/spotifyclone-80fcd.appspot.com/o/Seni%20Yerler.mp3?alt=media&token=12014c94-cd9b-429b-a528-13a58c989051")
-                 ])
-    )
-}
